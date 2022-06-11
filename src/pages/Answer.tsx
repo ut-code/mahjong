37c148fee916290012e0a1850d6c0f88206b1c69
brdgb <104970845+brdgb@users.noreply.github.com>
2022-06-11 17:26:50 +0900

    added radio option

--- src/pages/Answer.tsx
+++ src/pages/Answer.tsx
@@ -16,9 +16,21 @@ function CreateAnswer(){
             <form>
                 <p>回答内容</p>
                 <input type="text" name="replycontent" className="replybox"></input>
+                <div>
+                    <CreateRadioButton/>内容1
+                    <CreateRadioButton/>内容2
+                    <CreateRadioButton/>内容3
+                    <CreateRadioButton/>内容4
+                </div>
                 <input type="submit"></input>
             </form>
         </div>
+    )
+}
+
+function CreateRadioButton(){
+    return(
+        <input type="radio" name="option" value=""/>
     )
 }
 
