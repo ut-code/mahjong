b0d49f428cbae401e6e2b9c8295fb74a79e2bda7
waterwalker50 <104970808+waterwalker50@users.noreply.github.com>
2022-06-11 14:50:12 +0900

    homeの雛形を作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,35 @@
 import "../styles/Home.css"
 function Home() {
     // この辺でheaderを呼ぶ。
-    return <div className="question">これが箱になる予定</div>
+    return (
+    <div>
+    {/* データベースに存在する投稿の数だけCreatePreviewを呼ぶ操作 */}
+    <CreatePreview></CreatePreview>
+     <CreatePostButton></CreatePostButton>
+     </div>
+    )
 }
+
+function CreatePreview() {
+    return (
+    <div className="preview">
+        <div className="qtitle">ここにタイトル</div>
+        <div className="qproperty">
+            <div className="qimage">ここに画像</div>
+            <div className="qcomment">ここにコメント</div>
+        </div>
+        
+    </div>
+    )
+}
+
+function CreatePostButton(){
+    return (
+        <div className="postbutton">
+            質問を作成
+        </div>
+    )
+}
+
 
 export default Home;
