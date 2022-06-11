50350d06d034100b3661f59348adec81d6986e7a
taka2 <taka23131@gmail.com>
2022-06-11 13:02:50 +0900

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Home from './Home'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <Home></Home>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+            <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
