0c18418d42afa4d98e9ae70a38295b824b43a71e
taka2 <taka23131@gmail.com>
2022-06-11 15:35:26 +0900

    add api get /questions and post /questions

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const { PrismaClient } = require("@prisma/client")
+const client = new PrismaClient();
+
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.json({id: 1});
 });
+
+app.get('/questions', async (req, res) => {
+    const questions = await client.post.findMany();
+    res.json({posts: questions})
+});
+
+app.post('/questions', async (req, res) => {
+    const todos = await client.post.create({data: {body: res.body.body}})
+}) 
 
 app.listen('3500', () => {
     console.log('api started');
