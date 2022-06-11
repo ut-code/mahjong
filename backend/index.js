94608ba796401b78da70481afaf32f61a6aedcbd
taka2 <taka23131@gmail.com>
2022-06-11 19:05:50 +0900

    投稿ページを更新

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,32 @@
 const express = require("express");
 const app = express();
-const { PrismaClient } = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client");
 const client = new PrismaClient();
 
-app.use(express.urlencoded({extended: true}));
-app.use(function(req, res, next) {
+app.use(express.json());
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.get('/', (req, res) => {
-    res.json({id: 1});
+app.get("/", (req, res) => {
+  res.json({ id: 1 });
 });
 
-app.get('/questions', async (req, res) => {
-    const questions = await client.post.findMany();
-    res.json({posts: questions})
+app.get("/questions", async (req, res) => {
+  const questions = await client.post.findMany();
+  res.json({ posts: questions });
 });
 
-app.post('/questions', async (req, res) => {
-    const todos = await client.post.create({data: {body: res.body.body}})
-}) 
+app.post("/questions", async (req, res) => {
+  const post = await client.post.create({ data: { body: req.body.body } });
+  res.json({ post: post });
+});
 
-app.listen('3500', () => {
-    console.log('api started');
-})
+app.listen("3500", () => {
+  console.log("api started");
+});
