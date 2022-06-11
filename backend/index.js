3a066cf8f28312a74b946d2a06f892ee58f6af6b
taka2 <taka23131@gmail.com>
2022-06-11 16:59:26 +0900

    add fetcher to questions

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ const { PrismaClient } = require("@prisma/client")
 const client = new PrismaClient();
 
 app.use(express.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
     res.json({id: 1});
