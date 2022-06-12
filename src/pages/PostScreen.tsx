0e551bb0b98a03749a6080b6524791ddd49aec66
片倉大翔 <katakuraharuto@katakurahirotonoMacBook-Air.local>
2022-06-12 12:05:37 +0900

    Postscreenの見た目変更中

--- src/pages/PostScreen.tsx
+++ src/pages/PostScreen.tsx
@@ -18,8 +18,9 @@ function CreateCommentForm() {
   const [choices, setChoices] = useState<string[]>([]);
   const navigate = useNavigate();
   return (
-    <div>
-        <textarea
+    <div className="postBox">
+      <textarea
+        className="postText"
           id="comment"
           wrap="soft"
           value={text}
@@ -27,31 +28,34 @@ function CreateCommentForm() {
             setText(e.target.value);
           }}
         />
-        <input value={choice} onChange={e => {
-            setChoice(e.target.value)
-        }} />
-        <button onClick={() => {
-            setChoices([...choices, choice])
-            setChoice("")
-        }}>選択肢を追加</button>
-        <button
-          id="post-button"
-          onClick={async () => {
-            navigate("/");
-            const response = await fetch("http://localhost:3500/questions", {
-              method: "post",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ body: text, choices: choices }),
-            });
-          }}
-        >
-          送信
-        </button>
+        <div>
+          <input className="postOption" value={choice} onChange={e => {
+              setChoice(e.target.value)
+          }} />
+          <button className="postOpButton" onClick={() => {
+              setChoices([...choices, choice])
+              setChoice("")
+          }}>選択肢を追加</button>
+        </div>
       <ul>{
         choices.map((choice, index) => (
             <li key={index}>{choice}</li>
         ))
       }</ul>
+      <button
+        className="postButton"
+       id="post-button"
+        onClick={async () => {
+          navigate("/");
+          const response = await fetch("http://localhost:3500/questions", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ body: text, choices: choices }),
+          });
+        }}
+      >
+        送信
+      </button>
     </div>
   );
 }
